3446675cacc1cae9074581a477ff5406e75e5016
rmachaal <rahafam@hotmail.co.uk>
2024-06-23 20:20:03 +0100

    Deletes redundant page.

--- app/t/[id]/page.tsx
+++ app/t/[id]/page.tsx
@@ -1,60 +1,60 @@
-import { kv } from "@vercel/kv";
-import { notFound } from "next/navigation";
-// import FormRSC from "@/components/form-rsc";
-import { Metadata } from "next";
+// import { kv } from "@vercel/kv";
+// import { notFound } from "next/navigation";
+// // import FormRSC from "@/components/form-rsc";
+// import { Metadata } from "next";
 
-export async function generateMetadata({
-  params,
-}: {
-  params: {
-    id: string;
-  };
-}): Promise<Metadata | undefined> {
-  const data = await kv.hgetall<{ prompt: string; image?: string }>(params.id);
-  if (!data) {
-    return;
-  }
+// export async function generateMetadata({
+//   params,
+// }: {
+//   params: {
+//     id: string;
+//   };
+// }): Promise<Metadata | undefined> {
+//   const data = await kv.hgetall<{ prompt: string; image?: string }>(params.id);
+//   if (!data) {
+//     return;
+//   }
 
-  const title = `PrettyPlanner: ${data.prompt}`;
-  const description = `A spiral generated from the prompt: ${data.prompt}`;
+//   const title = `PrettyPlanner: ${data.prompt}`;
+//   const description = `A spiral generated from the prompt: ${data.prompt}`;
 
-  return {
-    title,
-    description,
-    openGraph: {
-      title,
-      description,
-    },
-    twitter: {
-      card: "summary_large_image",
-      title,
-      description,
-      creator: "@steventey",
-    },
-  };
-}
+//   return {
+//     title,
+//     description,
+//     openGraph: {
+//       title,
+//       description,
+//     },
+//     twitter: {
+//       card: "summary_large_image",
+//       title,
+//       description,
+//       creator: "@steventey",
+//     },
+//   };
+// }
 
-export default async function Results({
-  params,
-}: {
-  params: {
-    id: string;
-  };
-}) {
-  const data = await kv.hgetall<{
-    prompt: string;
-    pattern?: string;
-    image?: string;
-  }>(params.id);
+// export default async function Results({
+//   params,
+// }: {
+//   params: {
+//     id: string;
+//   };
+// }) {
+//   const data = await kv.hgetall<{
+//     prompt: string;
+//     pattern?: string;
+//     image?: string;
+//   }>(params.id);
 
-  if (!data) {
-    notFound();
-  }
-  return (
-    <FormRSC
-      prompt={data.prompt}
-      {...(data?.pattern && { pattern: data.pattern })}
-      image={data.image || null}
-    />
-  );
-}
+//   if (!data) {
+//     notFound();
+//   }
+//   return (
+//     <FormRSC
+//       prompt={data.prompt}
+//       {...(data?.pattern && { pattern: data.pattern })}
+//       image={data.image || null}
+//     />
+//   );
+// }
